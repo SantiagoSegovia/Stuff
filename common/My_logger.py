f91f15a07c22085887c31db4513168aef6f0eb76
Santiago Segovia <santiago.segovia-ext@continental-corporation.com>
2017-12-19 14:13:21 -0600

    ARGS stuff
    Added a template
    I do not remember if argsParser were before, but now it is. Simple
    functionallyti
    
    TODO: Parse the options into repective types
    
    Hanoi is faster
    
    My logger have levels for print and file.write. Also found bug when
    .exit into a try loop. Solved with tracing back just once.

--- common/My_logger.py
+++ common/My_logger.py
@@ -36,62 +36,84 @@ header = "         .-'''-.                                                 \n"\
 "'---'               ||     ||||     ||  `''-...... -'   | |      \n"\
 "                    \\'. __// \\'. __//                   |_|      \n"\
 "                     `'---'   `'---'                             \n"
+DEBUG = 4
+INFO = 3
+WARNING = 2
+ACTIONS = 1
 class Logger:
     class __Logger:
         def __init__(self, filename):
             filename = os.path.realpath(filename)
             self.output = filename
+            self.printLevel = INFO
+            self.writeLevel = DEBUG
             self.file = open(filename,"a")
             self.user = os.environ['COMPUTERNAME']
             self.date = time.strftime("%d/%m/%Y")
             self.hour = time.strftime("%I:%M:%S")
+            self.trace = False
             self.file.write("%s\n\nUser: %s\nDate: %s\nHour: %s\nFile: %s\nLogName: %s\n\n"%(header,self.user,self.date,self.hour,os.path.basename(log_name),self.output))
         def __str__(self):
             return self.output
         def debug(self,Msg):
             _str = "DEBUG:    %s"%(Msg)
-            self.file.write(_str + "\n")
-            print _str
+            if self.file != None and self.writeLevel >= DEBUG:
+                self.file.write(_str + "\n")
+            if (not self.trace) and self.printLevel >= DEBUG:
+                print _str
         def info(self,Msg):
             _str = "INFO:     %s"%(Msg)
-            self.file.write(_str + "\n")
-            print _str
+            if self.file != None and self.writeLevel >= INFO:
+                self.file.write(_str + "\n")
+            if (not self.trace) and self.printLevel >= INFO:
+                print _str
         def warning(self,Msg):
             _str = "WARNING:  %s"%(Msg)
-            self.file.write(_str + "\n")
-            print _str
+            if self.file != None and self.writeLevel >= WARNING:
+                self.file.write(_str + "\n")
+            if (not self.trace) and self.printLevel >= WARNING:
+                print _str
         def error(self,Msg, exit = False):
             _str = "ERROR:    %s"%(Msg)
-            self.file.write(_str + "\n")
-            print _str
+            if self.file != None and self.writeLevel >= ACTIONS:
+                self.file.write(_str + "\n")
+            if (not self.trace) and self.printLevel >= ACTIONS:
+                print _str
             if exit:
                 self.exit(exit_code = 50)
         def success(self,Msg, exit = False):
             _str = "SUCCESS:  %s"%(Msg)
-            self.file.write(_str + "\n")
-            print _str
+            if self.file != None and self.writeLevel >= ACTIONS:
+                self.file.write(_str + "\n")
+            if (not self.trace) and self.printLevel >= ACTIONS:
+                print _str
             if exit:
                 self.exit()
         def exit(self,exit_code = 0, Msg = "",erase = False):
             if Msg != "":
                 _str = "EXITTING: %s"%(Msg)
-                self.file.write(_str + "\n")
-                print _str
+                if self.file != None and self.writeLevel >= ACTIONS:
+                    self.file.write(_str + "\n")
+                if (not self.trace) and self.printLevel >= ACTIONS:
+                    print _str
             if exit_code == 50:
                 _str = "exit code 50 UNKNOWN ERROR"
-                self.file.write(_str + "\n")
-                print _str
+            elif exit_code == 20:
+                _str = "exit code 20 BAD ARGUMENT"
             elif exit_code == 12:
                 _str = "exit code 12 DIFF FILENAME"
-                self.file.write(_str + "\n")
-                print _str
             else:
                 _str = "exit code %i"%(exit_code)
+            if self.file != None and self.writeLevel >= ACTIONS:
                 self.file.write(_str + "\n")
+            if (not self.trace) and self.printLevel >= ACTIONS:
                 print _str
-            traceback.print_exc()
+            if not self.trace:
+                traceback.print_exc()
+                self.trace = True
             try:
                 self.file.close()
+                self.file = None
             except:
                 pass
             if erase:
---
+++ template.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#@     @@@@@@@@@@@@@@@@@@@@@@@@@@  @@@@@@@@@@@@@@@@@  @@@@@@@@@@@@@@@@@@@
+#  @@@  @@@@@@@@@@@@@@@ @@@@@@@@@  @@@@@@@@@@@@@@@@@  @@@@@@@@@@@@@@ @@@@
+#  @@@@@@@     @@@      @@@@@@@@@  @@@@@@  @@@  @@@@  @@@@     @@@@@ @@@@
+#@     @@  @@@  @  @@@  @@     @@      @@  @@@  @@      @  @@@  @@@@ @@@@
+#@@@@@  @       @@      @  @@@  @  @@@  @@      @@@@  @@@       @@@@ @@@@
+#  @@@  @  @@@@@@@@@@@  @  @@@  @  @@@  @@@@@@  @@@@  @@@  @@@@@@@@@@@@@@
+#@     @@@      @      @@@     @@      @@      @@@@@  @@@@      @@@@ @@@@
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#------------------------------------------------------------------------
+#| Author:   Santiago Segovia                                           |
+#| Title:    Script template                                            |
+#| Date:     19/12/2017                                                 |
+#| Help key: None                                                       |
+#------------------------------------------------------------------------
+import os
+import sys
+
+_my_path = os.path.dirname(__file__)
+sys.path.append(os.path.realpath(_my_path))
+import appendAll
+import My_logger as logger
+import argsParser as argsParser
+
+log = logger.get_log()
+parser = argsParser.parser
+
+def _parse_arguments():
+    log.info("Parsing arguments")
+    try:
+        parser.addAction(action_str = "--run", 
+                    shorcut = "-r", 
+                    dest = "run",
+                    help="Run such an important action.")
+        parser.addOption(option_str = "--path",
+                    shorcut="-p",
+                    dest="path",
+                    type=str,
+                    help="This is a very important directory for the script.",
+                    default=None)
+        parser.addOption(option_str = "--count",
+                    shorcut="-c",
+                    dest="numberOfPins",
+                    type=int,
+                    help="This is the number of pins of something.",
+                    default=0)
+        parser.addOption(option_str = "--demo",
+                    shorcut="-d",
+                    dest="isDemo",
+                    type=bool,
+                    help="True if only is a demo.",
+                    default=True)
+        parser.addOption(option_str = "--version",
+                    shorcut="-v",
+                    dest="version",
+                    type=float,
+                    help="Excpected in forma ##.##",
+                    default=1.0)
+        arguments = parser.parse()
+        return arguments
+    except:
+        log.error("Something were very bad parsing the arguments")
+        log.exit(20)
+
+def main():
+    try:
+        arguments = _parse_arguments()
+        print arguments.run
+        print arguments.path
+        print arguments.numberOfPins
+        print arguments.isDemo
+        print arguments.version
+    except:
+        log.error("Error in main application level.")
+        log.exit(50)
+
+if __name__ == "__main__":
+    main()
